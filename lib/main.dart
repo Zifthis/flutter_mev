31e76a9ec64fedadec744fb73a3f91a313f78c55
Matija Kovačević <matijakvcvc@gmail.com>
2020-12-10 20:55:24 +0100

    toolbar v1

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,77 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_mev/view/departments/menadzment.dart';
+import 'package:flutter_mev/view/departments/odrzivi_razvoj.dart';
+import 'package:flutter_mev/view/departments/racunarstvo.dart';
 import 'package:flutter_mev/view/main_screen.dart';
+import 'const/constants.dart';
 
 void main() {
   runApp(MevApp());
 }
 
-class MevApp extends StatelessWidget {
+class MevApp extends StatefulWidget {
+  @override
+  _MevAppState createState() => _MevAppState();
+}
+
+class _MevAppState extends State<MevApp> {
+  int _pageIndex = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
+  final MainScreen _mainScreen = MainScreen();
+  final Racunarstvo _racunarstvo = Racunarstvo();
+  final Menadzment _menadzment = Menadzment();
+  final OdrziviRazvoj _odrziviRazvoj = OdrziviRazvoj();
+
+  Widget _showPage = new MainScreen();
+
+  Widget _pageChoser(int page) {
+    switch (page) {
+      case 0:
+        return _mainScreen;
+        break;
+      case 1:
+        return _racunarstvo;
+        break;
+      case 2:
+        return _menadzment;
+        break;
+      case 3:
+        return _menadzment;
+        break;
+      case 4:
+        return _odrziviRazvoj;
+        break;
+      default:
+        return _mainScreen;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: MainScreen.id,
-      routes: {
-        MainScreen.id: (context) => MainScreen(),
-      },
+      home: Scaffold(
+        bottomNavigationBar: CurvedNavigationBar(
+          backgroundColor: gradientEndColor,
+          index: _pageIndex,
+          height: 65.0,
+          items: [
+            Icon(Icons.home, size: 30),
+            Icon(
+              Icons.add,
+              size: 30,
+            ),
+            Icon(Icons.list, size: 30),
+            Icon(Icons.compare_arrows, size: 30),
+          ],
+          onTap: (int tappedIndex) {
+            setState(() {
+              _showPage = _pageChoser(tappedIndex);
+            });
+          },
+        ),
+      ),
     );
   }
 }
--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -3,9 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mev/const/constants.dart';
 import 'package:flutter_mev/models/mev_models.dart';
 import 'package:flutter_mev/service/api_provider.dart';
+import 'package:flutter_mev/view/departments/menadzment.dart';
+import 'package:flutter_mev/view/departments/odrzivi_razvoj.dart';
+import 'package:flutter_mev/view/departments/racunarstvo.dart';
 import 'package:flutter_mev/view/detail_screen.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'package:flutter_mev/view/detail_screen.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class MainScreen extends StatefulWidget {
   static const String id = 'main_screen';
@@ -16,6 +20,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   Future<MevModels> _mevModels;
+
 
   @override
   void initState() {
@@ -52,195 +57,160 @@ class _MainScreenState extends State<MainScreen> {
                       ),
                       textAlign: TextAlign.left,
                     ),
-                    DropdownButton(
-                      items: [
-                        DropdownMenuItem(
-                          child: Text(
-                            'Smjerovi',
-                            style: TextStyle(
-                              fontFamily: 'Avenir',
-                              fontSize: 24,
-                              color: subTextColor,
-                              fontWeight: FontWeight.w500,
-                            ),
-                            textAlign: TextAlign.start,
-                          ),
-                        ),
-                      ],
-                      onChanged: (value) {},
-                      icon: Padding(
-                        padding: const EdgeInsets.only(left: 16.0),
-                        child: Image.asset('images/drop_down_icon.png'),
-                      ),
-                      underline: SizedBox(),
-                    ),
                   ],
                 ),
               ),
+              SizedBox(height: 15),
               Expanded(
-                child: Container(
-                  padding: const EdgeInsets.only(left: 32),
-                  child: FutureBuilder<MevModels>(
-                    future: _mevModels,
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        return Swiper(
-                          itemCount: snapshot.data.novosti.length,
-                          itemWidth: MediaQuery.of(context).size.width - 2 * 64,
-                          layout: SwiperLayout.STACK,
-                          pagination: SwiperPagination(
-                            builder: DotSwiperPaginationBuilder(
-                                activeSize: 20, space: 3),
-                            alignment: Alignment.bottomCenter,
-                          ),
-                          itemBuilder: (context, index) {
-                            var novosti = snapshot.data.novosti[index];
-                            return GestureDetector(
-                              onTap: () {
-                                Navigator.push(
-                                  context,
-                                  PageRouteBuilder(
-                                    pageBuilder: (context, a, b) =>
-                                        DetailScreen(
-                                      novosti: snapshot.data.novosti[index],
+                child: Center(
+                  child: Container(
+                    padding: const EdgeInsets.only(left: 32),
+                    child: FutureBuilder<MevModels>(
+                      future: _mevModels,
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return Swiper(
+                            itemCount: snapshot.data.novosti.length,
+                            itemWidth:
+                                MediaQuery.of(context).size.width - 2 * 64,
+                            layout: SwiperLayout.STACK,
+                            pagination: SwiperPagination(
+                              builder: DotSwiperPaginationBuilder(
+                                  color: Colors.white,
+                                  activeColor: secondaryTextColor,
+                                  activeSize: 22,
+                                  size: 10,
+                                  space: 2),
+                              alignment: Alignment.bottomCenter,
+                              margin: EdgeInsets.symmetric(vertical: 40.0),
+                            ),
+                            itemBuilder: (context, index) {
+                              var novosti = snapshot.data.novosti[index];
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(
+                                    context,
+                                    PageRouteBuilder(
+                                      pageBuilder: (context, a, b) =>
+                                          DetailScreen(
+                                        novosti: snapshot.data.novosti[index],
+                                      ),
                                     ),
-                                  ),
-                                );
-                              },
-                              child: Stack(
-                                children: [
-                                  Column(
-                                    children: [
-                                      SizedBox(height: 100),
-                                      Card(
-                                        elevation: 8.0,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(32),
-                                        ),
-                                        color: Colors.white,
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(32.0),
-                                          child: Column(
-                                            crossAxisAlignment:
-                                                CrossAxisAlignment.start,
-                                            children: [
-                                              SizedBox(height: 100),
-                                              Text(
-                                                novosti.naslov,
-                                                style: TextStyle(
-                                                  fontFamily: 'Avenir',
-                                                  fontSize: 27,
-                                                  color:
-                                                      const Color(0xFF47455F),
-                                                  fontWeight: FontWeight.w900,
+                                  );
+                                },
+                                child: Stack(
+                                  children: [
+                                    Column(
+                                      children: [
+                                        SizedBox(height: 100),
+                                        Card(
+                                          elevation: 8.0,
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(32),
+                                          ),
+                                          color: Colors.white,
+                                          child: Padding(
+                                            padding: const EdgeInsets.all(32.0),
+                                            child: Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                SizedBox(height: 100),
+                                                Text(
+                                                  novosti.naslov,
+                                                  maxLines: 1,
+                                                  style: TextStyle(
+                                                    fontFamily: 'Avenir',
+                                                    fontSize: 25,
+                                                    color:
+                                                        const Color(0xFF47455F),
+                                                    fontWeight: FontWeight.w900,
+                                                  ),
+                                                  textAlign: TextAlign.left,
                                                 ),
-                                                textAlign: TextAlign.left,
-                                              ),
-                                              Text(
-                                                novosti.podNaslov,
-                                                style: TextStyle(
-                                                  fontFamily: 'Avenir',
-                                                  fontSize: 18,
-                                                  color: primaryTextColor,
-                                                  fontWeight: FontWeight.w500,
+                                                Text(
+                                                  novosti.podNaslov,
+                                                  maxLines: 2,
+                                                  style: TextStyle(
+                                                    fontFamily: 'Avenir',
+                                                    fontSize: 18,
+                                                    color: primaryTextColor,
+                                                    fontWeight: FontWeight.w500,
+                                                  ),
+                                                  textAlign: TextAlign.left,
                                                 ),
-                                                textAlign: TextAlign.left,
-                                              ),
-                                              SizedBox(height: 12),
-                                              Row(
-                                                children: [
-                                                  Text(
-                                                    'Prikaži detalje',
-                                                    style: TextStyle(
-                                                      fontFamily: 'Avenir',
-                                                      fontSize: 13,
-                                                      color: secondaryTextColor,
-                                                      fontWeight:
-                                                          FontWeight.w500,
+                                                SizedBox(height: 12),
+                                                Row(
+                                                  children: [
+                                                    Text(
+                                                      'Prikaži detalje',
+                                                      style: TextStyle(
+                                                        fontFamily: 'Avenir',
+                                                        fontSize: 13,
+                                                        color:
+                                                            secondaryTextColor,
+                                                        fontWeight:
+                                                            FontWeight.w500,
+                                                      ),
+                                                      textAlign: TextAlign.left,
                                                     ),
-                                                    textAlign: TextAlign.left,
-                                                  ),
-                                                  Icon(
-                                                    Icons.arrow_forward,
-                                                    color: secondaryTextColor,
-                                                  ),
-                                                ],
-                                              ),
-                                            ],
+                                                    Icon(
+                                                      Icons.arrow_forward,
+                                                      color: secondaryTextColor,
+                                                    ),
+                                                  ],
+                                                ),
+                                              ],
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                  Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceEvenly,
-                                    children: [
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(top: 15.0),
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                            color: Colors.white,
-                                            shape: BoxShape.circle,
-                                            boxShadow: [
-                                              BoxShadow(
-                                                  blurRadius: 8,
-                                                  color: Colors.blueGrey,
-                                                  spreadRadius: 3)
-                                            ],
-                                          ),
-                                          child: CircleAvatar(
-                                            radius: 95.0,
-                                            backgroundImage:
-                                                NetworkImage(novosti.slika),
-                                            backgroundColor: Colors.transparent,
+                                      ],
+                                    ),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceEvenly,
+                                      children: [
+                                        Padding(
+                                          padding:
+                                              const EdgeInsets.only(top: 15.0),
+                                          child: Container(
+                                            decoration: BoxDecoration(
+                                              color: Colors.white,
+                                              shape: BoxShape.circle,
+                                              boxShadow: [
+                                                BoxShadow(
+                                                    blurRadius: 8,
+                                                    color: Colors.blueGrey,
+                                                    spreadRadius: 3)
+                                              ],
+                                            ),
+                                            child: CircleAvatar(
+                                              radius: 95.0,
+                                              backgroundImage:
+                                                  NetworkImage(novosti.slika),
+                                              backgroundColor:
+                                                  Colors.transparent,
+                                            ),
                                           ),
                                         ),
-                                      ),
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            );
-                          },
-                        );
-                      } else {
-                        return Center(child: CircularProgressIndicator());
-                      }
-                    },
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          );
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
+                    ),
                   ),
                 ),
               ),
             ],
           ),
-        ),
-      ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.vertical(
-            top: Radius.circular(36.0),
-          ),
-          color: navigationColor,
-        ),
-        padding: const EdgeInsets.all(15.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            IconButton(
-              icon: Image.asset('images/menu_icon.png'),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Image.asset('images/search_icon.png'),
-              onPressed: () {},
-            ),
-            IconButton(
-              icon: Image.asset('images/profile_icon.png'),
-              onPressed: () {},
-            ),
-          ],
         ),
       ),
     );
